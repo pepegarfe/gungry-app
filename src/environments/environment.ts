4085f86d725c6d75628469cc6525396e0c6c47d7
pepegarfe <pepegarfe@hotmail.com>
2022-10-16 16:38:55 -0500

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  production: false,
+  serverUrl: 'http://localhost:1337/api',
+  appUrl: 'https://trynearme.app',
+  appImageUrl: 'https://trynearme.app/assets/img/nearme.png',
+  appId: 'myAppId',
+  fbId: 'YOUR_FB_ID',
+  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
+  androidHeaderColor: '#d82c6b',
+  defaultUnit: 'mi',
+  defaultLang: 'en',
+  googleClientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com',
+  oneSignal: {
+    appId: 'YOUR_ONESIGNAL_APP_ID',
+  },
+  currency: {
+    code: 'USD',
+    display: 'symbol',
+    digitsInfo: '1.2-2',
+  },
+};
