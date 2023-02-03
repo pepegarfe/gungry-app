f9586b651c42c8d95395d1f5170e53ce65eca606
pepegarfe <114194987+pepegarfe@users.noreply.github.com>
2023-02-03 01:11:49 -0600

    Update tabs.page.ts

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -63,7 +63,7 @@ export class TabsPage extends BasePage implements OnInit {
 
   ngOnInit() {
 
-    this.tabNames = ['home', 'explore', 'posts', 'map', 'profile'];
+    this.tabNames = ['home', 'explore', 'posts', 'map', 'likes', 'profile'];
 
     this.topLevelRoutes = [
       '/',
@@ -72,6 +72,7 @@ export class TabsPage extends BasePage implements OnInit {
       '/1/explore',
       '/1/posts',
       '/1/home/places/map',
+      '/1/profile/likes',
       '/1/profile',
     ];
 
